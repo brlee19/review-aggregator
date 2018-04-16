857578e2a8b35be2af7b7351d7a6beb2e9bf2c4b
brianlee <blee@nyu.edu>
2018-04-16 17:58:35 -0400

    Add test API calls to Yelp

---
+++ helpers/yelp.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const token = require('../config.js').yelp_api_key;
+
+const searchPlacesByCoords = (coords, query) => {
+  const tokenHeader = {'Authorization': 'Bearer ' + token};
+  const config = {
+    latitude: coords.lat, //to number?
+    longitude: coords.lng,
+    radius: 500,
+    categories: query.categories, //may need to map google to yelp cats, cats are optional for yelp
+    term: query.term
+  };
+  return axios.get('https://api.yelp.com/v3/businesses/search', {headers: tokenHeader, params: config})
+    .then(resp => {
+      return resp.data;
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+exports.searchPlacesByCoords = searchPlacesByCoords;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const google = require('../helpers/google.js');
+const yelp = require('../helpers/yelp.js');
 const port = 3000;
 
 const app = express();
@@ -8,10 +9,16 @@ app.listen(port, () => console.log(`listening on port ${port}!`));
 app.use(bodyParser.json());
 
 app.get('/test', (req, res) => {
-  google.searchPlacesByAddress('350 E 30th Street New york', {type: 'restaurant', keyword: 'sushi'})
-    .then(places => {
-      console.log('places are', places)
-      res.send(places);
-    })
-    .catch(err => res.send(err));
+  // google.searchPlacesByAddress('350 E 30th Street New york', {type: 'restaurant', keyword: 'sushi'})
+  //   .then(places => {
+  //     console.log('places are', places)
+  //     res.send(places);
+  //   })
+  //   .catch(err => res.send(err));
+  google.convertAddressToCoords('350 E 30th St, New York NY')
+    .then((coords => {
+      yelp.searchPlacesByCoords(coords, {categories: 'restaurants', term: 'sushi'})
+        .then(results => res.send(results))
+        .catch(err => res.send(err))
+    }))
 })
