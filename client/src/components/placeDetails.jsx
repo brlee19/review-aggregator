e975736e0d90cac0841b6804a8218a1ecb1825d5
brianlee <blee@nyu.edu>
2018-04-21 06:14:00 -0400

    Add restaurant hours

--- client/src/components/placeDetails.jsx
+++ client/src/components/placeDetails.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//component only is aware of yelp info
 
 const detailsStyle = {
 	borderWidth: '1px 1px',
@@ -24,8 +23,6 @@ class PlaceDetails extends React.Component {
   constructor(props) { //need to render a lot of details based on whether the details exist
     super(props);
     console.log(props.place);
-    // this.state = {}; this.props.location.places would be the places
-    // this.getLiStyle = this.getLiStyle.bind(this);
   }
 
   render() {
@@ -42,25 +39,40 @@ class PlaceDetails extends React.Component {
         {this.props.place.baseData.location.display_address.join(', ')}
         <br></br>
         {this.props.place.baseData.display_phone}
+        {(this.props.place.foursquare.hours && this.props.place.foursquare.hours.timeframes.length) ? (
+          <div>
+          <h4>hours</h4>
+          <div style={smaller}>
+            {this.props.place.foursquare.hours.timeframes.map((timeframe, i) => {
+              return <div key={i}>{timeframe.days} : {timeframe.open.map((openTime, j) => <span key={j}>{openTime.renderedTime}  </span>)}</div>
+            })}
+          </div>
+          </div>
+        ) : <span></span>}
         <h4>stats</h4>
         <div style={smaller}>
-          Yelp Rating: {this.props.place.baseData.rating} out of 5  ||
-          Google Rating: {this.props.place.google.rating} out of 5  ||
-          Foursquare Rating: {this.props.place.foursquare.rating} out of 10  
+          Yelp Rating: {this.props.place.baseData.rating || 'not available'} out of 5  ||
+          Google Rating: {this.props.place.google.rating || 'not available'} out of 5  ||
+          Foursquare Rating: {this.props.place.foursquare.rating ? `${this.props.place.foursquare.rating} out of 10` : 'not available'}  
         </div>
         <div style={smaller}>
           Yelp Price: {this.props.place.baseData.price.length || 'not available'}  ||
           Google Price: {this.props.place.google.price_level || 'not available'}  ||
-          Foursquare Price: {this.props.place.foursquare.price.tier}
+          Foursquare Price: {this.props.place.foursquare.price.tier || 'not available'}
         </div>
 
         <h4>links</h4>
         <div style={smaller}>
-          {this.props.place.foursquare.menu ? <span><a href={this.props.place.foursquare.menu.mobileUrl} target="_blank">Menu Mobile</a>  || </span>: <div></div>}
-          {this.props.place.foursquare.menu ? <span><a href={this.props.place.foursquare.menu.url} target="_blank">Menu</a>  || </span> : <div></div>}
+          {this.props.place.foursquare.menu ? (
+            <span><a href={this.props.place.foursquare.menu.mobileUrl} target="_blank">Menu Mobile</a>  || </span>
+          ): <div></div>}
+          {this.props.place.foursquare.menu ? (
+            <span><a href={this.props.place.foursquare.menu.url} target="_blank">Menu</a>  || </span>
+          ): <div></div>}
           <a href={this.props.place.foursquare.url} target="_blank">Restaurant Home Page</a>  ||
-          <a href={this.props.place.foursquare.shortUrl} target="_blank">Restaurant Foursquare Page</a>  ||
-          <a href={this.props.place.baseData.url} target="_blank">Restaurant Yelp Page</a>
+          <a href={this.props.place.foursquare.shortUrl} target="_blank">Foursquare Page</a>  ||
+          <a href={this.props.place.baseData.url} target="_blank">Yelp Page</a> 
+          {this.props.place.foursquare.delivery ? <span> ||  <a href={this.props.place.foursquare.delivery.url} target="_blank">Order Delivery</a></span> : <span></span>}
         </div>
 
         <h4>foursquare tips</h4>
@@ -72,7 +84,6 @@ class PlaceDetails extends React.Component {
                     <a href={review.url} target="_blank">Read the full review</a>
                   </div>)
         })}
-        {/*as long as place's props only go one deep, everything will be blank until something is selected*/}
       </div>
     ) :
     (
